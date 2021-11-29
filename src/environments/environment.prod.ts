6b35c5cd00d3d63d271d574b7497e4183f610175
NataHanguga <ptahakaimanka@yandex.ru>
2021-11-29 16:12:30 +0300

    fixed prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // url: 'https://diploma-server-01.herokuapp.com/',
-  url: 'http://localhost:3000/'
+  url: 'https://diploma-server-01.herokuapp.com/',
+  // url: 'http://localhost:3000/'
 
 };
